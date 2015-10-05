4d7dd184d49a0826f5b2da22248d0eb1982bb018
Luke Clifton <ltclifton@gmail.com>
2015-10-05 18:48:56 +1100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # irssi-na
-Pushover integration for irssi
+
+Pushover integration for irssi.
+
+## Getting Started
+
+Copy the `na.pl` script into your ~/.irssi/scripts/` directory.
+
+You will need the following perl libraries
+
+    perl-libwww-perl-6.13
+    perl-LWP-Protocol-https-6.04
+
+
+Get an account at https://pushover.net and create a new
+application token.
+
+In irssi
+
+    /set pushover_app_token <token>
+    /set pushover_user_token <token>
+    /run na
+
+You will now get a pushover notification when someone mentions you
+or sends you a private message.
+
---
+++ na.pl
@@ -0,0 +1,37 @@
+use strict;
+
+use Irssi;
+use LWP::UserAgent;
+
+sub naAlert {
+  my ($server, $msg, $nick, $addr, $target) = @_;
+  my $title = ("$target" eq "" ? "$nick to $server->{nick}" : "$nick to $target");
+  my $token = Irssi::settings_get_str("pushover_app_token");
+  my $user  = Irssi::settings_get_str("pushover_user_token");
+
+  if ($token eq "") {
+    print "pushover_app_token not set";
+  }
+  else {
+    if ($user eq "") {
+      print "pushover_user_token not set";
+    }
+    else {
+      if (($msg =~ $server->{nick}) || ("$target" eq "")) {
+        LWP::UserAgent->new()->post(
+          "https://api.pushover.net/1/messages.json", [
+            "token"   => $token,
+            "user"    => $user,
+            "message" => $msg,
+            "title"   => $title,
+           ]);
+      }
+    }
+  }
+};
+
+Irssi::settings_add_str("pushover", "pushover_app_token", "");
+Irssi::settings_add_str("pushover", "pushover_user_token", "");
+
+Irssi::signal_add_last("message public", 'naAlert');
+Irssi::signal_add_last("message private", 'naAlert');
